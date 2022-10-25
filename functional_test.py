daafccddd6ec8e72badbd78843ac18ddff6f04dd
Justinkay0 <14783039+Justinkay0@users.noreply.github.com>
2022-10-25 19:36:57 +0800

    First commit: First functional test and basic django config

---
+++ functional_test.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+driver = webdriver.Firefox(options=options)
+driver.get('http://localhost:8000')
+
+assert 'Django' in driver.title
